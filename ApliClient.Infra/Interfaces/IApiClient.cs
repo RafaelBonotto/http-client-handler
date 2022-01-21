8206aa231b946712732e372f12d013febd91e05b
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-01-21 18:10:28 -0300

    Create IApiClient.cs

---
+++ ApliClient.Infra/Interfaces/IApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ApliClient.Infra.Impl;
+
+namespace ApliClient.Infra.Interfaces
+{
+    public interface IApiClient
+    {
+        Task<RespostaServico<T>> PostAsync<T>(object item, string url, List<KeyValuePair<string, string>> headers);
+        Task<RespostaServico<T>> GetAsync<T>(string url, List<KeyValuePair<string, string>> headers);
+        Task<RespostaServico<T>> PutAsync<T>(object item, string url, List<KeyValuePair<string, string>> headers);
+        Task<RespostaServico<T>> DeleteAsync<T>(object item, string url, List<KeyValuePair<string, string>> headers);
+    }
+}
